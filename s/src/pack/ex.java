665c4825dfa84239ffa5060fd6e0a4bcd0a10404
Lab <Lab@Qa23>
2017-07-06 14:37:08 +0530

    file

---
+++ s/src/pack/ex.java
@@ -0,0 +1,39 @@
+package pack;
+
+public class ex {
+
+	public void main1()
+	{
+		System.out.println("main1");
+	}
+	
+	public void main2()
+	{
+		System.out.println("main2");
+	}
+	public void main3()
+	{
+		System.out.println("main3");
+	}
+	public void main4()
+	{
+		System.out.println("main4");
+	}
+	
+	public static void main(String[] args) {
+		ex a=new ex();
+		a.main1();
+		a.main2();
+		a.main3();
+		a.main4();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
